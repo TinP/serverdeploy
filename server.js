2a50fceae2cd9ebcb211e7ac6130cf14b69098b9
Tin Partomnupong <tinpartomnupong@Tins-MacBook-Pro.local>
2016-04-24 13:59:39 +0700

    update code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res){
   res.send('hello world');
 });
@@ -12,4 +14,10 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 })
 
-app.listen(503);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
