01545791c3dd21ae86b18292449aa05b512651eb
Tin Partomnupong <tinpartomnupong@Tins-MacBook-Pro.local>
2016-04-24 13:39:43 +0700

    commit eieid

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'evertonfc') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(3000);
