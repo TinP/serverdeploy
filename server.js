1b1eb8fdc2bf4eccea2b2c30f793889edfd50744
Tin Partomnupong <tinpartomnupong@Tins-MacBook-Pro.local>
2016-04-24 14:37:50 +0700

    tukky

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
-
+var request = require('request')
+app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
@@ -13,6 +15,43 @@ app.get('/webhook/', function (req, res) {
   }
   res.send('Error, wrong validation token');
 })
+
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+  var  event = req.body.entry[0].messaging[i];
+  var  sender = event.sender.id;
+    if (event.message && event.message.text) {
+     var text = event.message.text;
+      // Handle a text message from this sender
+      sendTextMessage(sender, 'TUKKY : ' + text)
+    }
+  }
+  res.sendStatus(200);
+});
+
+var token = "ah5s4PEjMzwa2QZB1Kd306ybAnf7mxhJsd4fqdiHP3vDHa1sX2pz9bPRIVjSEAZD";
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
